5c09c9d02f521dfb7bc8cccfe177da21741e7b73
spookeyy <pangasmeshack@gmail.com>
2024-05-20 18:48:57 +0300

    tuples

---
+++ Tuples.py
@@ -0,0 +1,14 @@
+#tuples
+# Tuples are nearly identical to lists, except that they are immutable
+# Tuples are created with the tuple() function
+# Tuples are created with round brackets
+
+# Tuples are immutable. once created, they cannot be changed
+
+(1, 2, 3)
+# => (1, 2, 3)
+tuple([1, 2, 3])
+# => (1, 2, 3)
+
+# used in in data retrieved from a database. 
+# Since you want to keep an accurate record of what is in the database while your application works with the data, a tuple will protect that information until it is no longer needed.
