687b8906d343e32d489be7978a032d04f4a1a6d5
spookeyy <pangasmeshack@gmail.com>
2024-05-20 19:11:05 +0300

    Booleans

---
+++ Booleans.py
@@ -0,0 +1,19 @@
+#booleans
+not True
+# => False
+not False
+# => True
+not 1
+# => False
+not 0
+# => True
+not ''
+# => True
+not None
+# => True
+not []
+# => True
+not ()
+# => True
+not {}
+# => True
