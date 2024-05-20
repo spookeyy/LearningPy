cc0ab9edc8cc9d8c9e7335f4d860fa866ffc5e34
spookeyy <pangasmeshack@gmail.com>
2024-05-20 18:38:17 +0300

    pop in lists and append

---
+++ Lists.py
@@ -0,0 +1,40 @@
+[1, 3, 400, 7]
+# => [1, 3, 400, 7]
+
+#creating an empty list automatically
+list()
+# => []
+
+list_abc = ['a', 'b', 'c']
+list_abc[0]
+# => 'a'
+list_abc[1]
+# => 'b'
+list_abc[2]
+# => 'c'
+
+list_abc[0] = 'z' #replaces 'a' with 'z'
+list_abc
+# => ['z', 'b', 'c']
+
+list_abc.append('d')
+list_abc
+# => ['z', 'b', 'c', 'd']
+
+
+len([1, 3, 400, 7])
+# => 4
+
+sorted([5, 100, 234, 7, 2])
+# => [2, 5, 7, 100, 234]
+
+sorted([5, 100, 234, 7, 2], reverse=True)
+# => [234, 100, 7, 5, 2]
+
+sorted([5, 100, 234, 7, 2], key=lambda x: x % 10) #sorts by last digit of each number
+# => [5, 7, 2, 100, 234]
+
+list_123 = [1, 2, 3]
+list_123.append(4)
+list_123
+# => [1, 2, 3, 4]
---
+++ pop.py
@@ -0,0 +1,11 @@
+# pop 
+list_123 = [1, 2, 3]
+list_123.pop()
+# => 3
+print(list_123)
+# => [1, 2]
+
+list_123.pop(0)
+# => 1
+print(list_123)
+# => [2]
