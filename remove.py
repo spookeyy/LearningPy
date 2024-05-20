701d546d42de1ba3a32afb96522d8251dba9d59d
spookeyy <pangasmeshack@gmail.com>
2024-05-20 18:42:46 +0300

    remove

---
+++ remove.py
@@ -0,0 +1,10 @@
+#remove()
+#removes an element from a list
+#remove() is used to remove an element from a list
+#syntax
+#list_name.remove(element)
+
+list_abc = ['a', 'b', 'c']
+list_abc.remove('b')
+print(list_abc)
+# => ['a', 'c']
