fc017b59db14128944c074cee505e37805f25eae
spookeyy <pangasmeshack@gmail.com>
2024-05-26 02:53:20 +0300

    class

--- OOP/CLASS.py
+++ OOP/CLASS.py
@@ -15,22 +15,22 @@ class Animal:
     # constructor - init.
     def __init__(self, my_name, my_age):
         # attribute - instance variable
-        self.name = my_name
-        self.age = my_age
+        self.name = my_name.title() # capitalize first letter
+        self.my_age = my_age
 
     def printer(self):
-        print(f"My name is {self.name} and I am {self.age} years old")
+        print(f"My name is {self.name} and I am {self.my_age} years old")
 
     def age(self): # instance method
         print("I am 10 years old")
 
 
 # object is an instance of a class
-cat = Animal("kitty", 10)
+cat = Animal("mitty", 10)
 cat.printer()
 # cat.age()
 
-dog = Animal("puppy", 5)
+dog = Animal("huppy", 5)
 dog.printer()
 # dog.age()
 
@@ -59,3 +59,22 @@ class Dog(Animal):
         print(f"My name is {self.name} and I am {self.age} years old and I am a {self.breed}")
 
 
+# Dog in dog.py prints "Name must be string between 1 and 25 characters." if empty string. - AssertionError: assert '' == 'Name must be...characters.\n'
+# solution
+
+# class Dog(Animal):
+#     def __init__(self, my_name, my_age, my_breed):
+#         super().__init__(my_name, my_age)
+#         if my_name == "":
+#             raise ValueError("Name must be string between 1 and 25 characters.")
+#         self.breed = my_breed
+
+
+# string to title case
+# syntax
+# string.title()
+
+# example
+string = "guido van rossum"
+string.title()
+# => "Guido Van Rossum"
