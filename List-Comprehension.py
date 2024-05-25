829009cfd51c96acf02efd2bd07483654a672932
spookeyy <pangasmeshack@gmail.com>
2024-05-25 09:23:26 +0300

    list comprehension

--- List-Comprehension.py
+++ List-Comprehension.py
@@ -17,3 +17,19 @@ squared_minus_one = [(n ** 2) - 1 for n in range(1, 11)]
 print(squared_minus_one)
 # => [0, 3, 8, 15, 24, 35, 48, 63, 80, 99]
 
+# FROM TESTS IN CANVAS
+def return_evens(num_list):
+    #returns empty list when num_list has no evens - assert None == []
+    for n in num_list:
+        if n % 2 != 0:
+            return []
+        # returns evens from num_list - assert [] == [0, 2, 4, 6, 8]
+        return [n for n in num_list if n % 2 == 0]
+    return [n for n in num_list if n % 2 == 0]
+
+def make_exclamation(sentence_list):
+    #returns empty list when sentence_list is empty - assert [] == []
+    if not sentence_list:
+        return []
+    # returns list of sentences with exclamation marks
+    return [sentence + "!" for sentence in sentence_list]
