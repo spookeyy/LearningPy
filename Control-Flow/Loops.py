a5d3e147797b6e85b02535ad8c6bc2271bf4e9cc
spookeyy <pangasmeshack@gmail.com>
2024-05-25 11:20:43 +0300

    loops

--- Control-Flow/Loops.py
+++ Control-Flow/Loops.py
@@ -41,3 +41,42 @@ for height in player_heights:
 # simple version (comprehension)
 inch_heights = [height * 7920 for height in player_heights]
 print(inch_heights)
+
+
+# for loop with index
+for i in range(len(inch_heights)):
+    print(inch_heights[i], "is at index ", i)
+
+# check if item is in list or not
+for height in inch_heights:
+    if height == 7920:
+        print("Found")
+        break
+else:
+    print("Not Found")
+
+#FROM CLASS TEST CANVAS CONTENT.
+def happy_new_year():
+    # code goes here!
+    x = 10
+    while x > 0:
+        print(x)
+        x -= 1
+    print("Happy New Year!")
+
+def square_integers(int_list):
+    # code goes here!
+    new_list = [integer ** 2 for integer in int_list]
+    return new_list
+
+def fizzbuzz():
+    # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
